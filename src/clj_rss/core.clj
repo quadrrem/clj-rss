53e8616cc954a262d318bebd7f8178673b070373
quadrrem <quadrelli@Remos-MacBook-Pro.local>
2017-09-08 19:08:25 +0200

    read rss xml to map structure

--- src/clj_rss/core.clj
+++ src/clj_rss/core.clj
@@ -3,24 +3,34 @@
            [clojure.xml :as xml]
            [clojure.pprint :as pp]))
 
-(def URL "https://www.srf.ch/news/bnf/rss/1890")
-
 (defn parse [url]
   (with-open
-    [xin (io/input-stream (java.net.URL. url))]
+    [xin (io/input-stream url)]
       (xml/parse xin)))
-     
+
+(defn is-item? [x]
+  (contains? x :item))
+
+(defn conj-item [x , y]
+  (let [items (:items x)]
+    (conj x {:items (conj items (vec y))})))
+
+(defn conj-channel 
+  ([] [])
+  ([x y] (if (is-item? y)
+            (conj-item x y)
+            (conj x y))))
+
 (defn read-tags [el]
   (when (not (nil? el))
     (if (map? el)
       (let [tag (:tag el)
             content (:content el)]
-        {(keyword tag) (reduce conj (map read-tags content))})
+        {(keyword tag) (reduce conj-channel (map read-tags content))})
       el)))
 
-(defn test []
-  (pp/pprint (read-tags (parse URL))))
+(defn read-rss [source]
+  (read-tags (parse source)))
 
-(defn -main [& args]
-  (println "Working!")
-  (test))
+(defn- main [& args]
+  (pp/pprint (read-rss (first args))))
--- test/clj_rss/core_test.clj
+++ test/clj_rss/core_test.clj
@@ -1,7 +1,23 @@
 (ns clj-rss.core-test
   (:require [clojure.test :refer :all]
-            [clj-rss.core :refer :all]))
+            [clj-rss.core :refer :all]
+            [clojure.pprint :as pp]
+            [clojure.java.io :as io]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def URL "https://www.srf.ch/news/bnf/rss/1890")
+(def FILE "testrss.xml")
+
+;(deftest read-test-url
+;  (testing "Read test url rss"
+;    (pp/pprint (read-rss URL))))
+
+;(deftest print-test-file
+;  (testing "Read test file rss"
+;    (pp/pprint (read-rss (io/resource FILE)))))
+
+(deftest read-test-file
+  (testing "Read test file rss"
+    (let [channel (get-in (read-rss (io/resource FILE)) [:rss :channel])]
+      (is (= 48 (count (:items channel))))
+      (is (= "SRF News Schweiz" (:title channel)))
+      (is (= "https://www.srf.ch/news/bnf/rss/1890" (:link channel))))))
