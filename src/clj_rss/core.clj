77c47ec933b3603e9349eb256511c0cb31f8f2e9
quadrrem <quadrelli@Remos-MacBook-Pro.local>
2017-09-08 07:53:17 +0200

    read XMl and parse map

--- src/clj_rss/core.clj
+++ src/clj_rss/core.clj
@@ -1,6 +1,26 @@
-(ns clj-rss.core)
+(ns clj-rss.core (:gen-class)
+  (require [clojure.java.io :as io]
+           [clojure.xml :as xml]
+           [clojure.pprint :as pp]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def URL "https://www.srf.ch/news/bnf/rss/1890")
+
+(defn parse [url]
+  (with-open
+    [xin (io/input-stream (java.net.URL. url))]
+      (xml/parse xin)))
+     
+(defn read-tags [el]
+  (when (not (nil? el))
+    (if (map? el)
+      (let [tag (:tag el)
+            content (:content el)]
+        {(keyword tag) (reduce conj (map read-tags content))})
+      el)))
+
+(defn test []
+  (pp/pprint (read-tags (parse URL))))
+
+(defn -main [& args]
+  (println "Working!")
+  (test))
